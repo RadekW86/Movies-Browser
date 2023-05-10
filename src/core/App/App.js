4ebd07c561b812e62267da0d29d581c4b7a68ff0
Jakub Jachacz <hexadoxpl@gmail.com>
2023-05-10 15:14:26 +0200

    put Navigation in App temporarily

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,6 +1,6 @@
 import { Container } from "../../common/Container/styled";
+import Navigation from "../../common/Navigation";
 
 export const App = () => (
-  <Container>
-  </Container>
+ <Navigation /> 
 );
