0e5a0f4e8b4caefca9fc1be6ce6347e41fc01009
Radek Włodarski <wlodarski.radek@gmail.com>
2023-05-10 16:36:58 +0200

    added a no-results content component

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,12 +1,13 @@
 import { Container } from "../../common/Container/styled";
 import { Section } from "../../common/Section";
+import { NoResults } from "../../common/content/NoResults";
 import { Tiles } from "../../common/content/Tiles";
 
 export const App = () => (
   <Container>
     <Section
-      title="Example Title"
-      content={<Tiles />}
+      title="Sorry, there are no results for “Muan”"
+      content={<NoResults />}
       foot="EXAMPLE FOOT"
     />
   </Container>
