28f76118f86dbb08dd9ab054f42bb40c5f5cdfe8
Jakub Jachacz <hexadoxpl@gmail.com>
2023-05-11 17:09:41 +0200

    add more theme colors

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,6 +1,10 @@
 export const theme = {
     colors: {
+        black: "#18181B",
+        blue: "#0044CC",
         mercury: "#E5E5E5",
+        white: "#FFFFFF",
+        darkergray: "#7E839A",
     },
     breakPoint: {
         mobileMax: 767,
