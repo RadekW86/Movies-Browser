a28beb100f07009da3a07f8bbc7ec1e018c23067
Jakub Jachacz <hexadoxpl@gmail.com>
2023-05-11 17:09:21 +0200

    updated Search component styling

--- src/common/Navigation/Search/styled.js
+++ src/common/Navigation/Search/styled.js
@@ -1,16 +1,20 @@
 import styled from "styled-components";
-import {ReactComponent as SearchImage} from "../../images/search.svg";
+import SearchImage from "../../images/search.svg";
 
 export const Search = styled.input` 
 height: 48px;
-width: 432px;
+width: 365px;
 border-radius: 33px;
-margin-left: 311px;
+margin-left: 380px;
 margin-right: 292px;
 border: 1px solid #E4E6F0;
 background-image: url(${SearchImage});
 background-repeat: no-repeat;
-background-position: 10px center;
-padding-left: 40px;
-background-size: contain; 
+background-position: 27px center;
+padding-left: 63px;
+background-size: auto;  
+
+&::placeholder {
+    color: ${({ theme }) => theme.colors.darkergray}
+}
 `
