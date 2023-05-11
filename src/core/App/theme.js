8ba89326e984d189957d9addc89f5ccc68393be9
Jakub Jachacz <hexadoxpl@gmail.com>
2023-05-11 21:07:57 +0200

    add more theme colors

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,6 +1,12 @@
 export const theme = {
     colors: {
         mercury: "#E5E5E5",
+        black: "#181818",
+        grey: "#E4E6F0",
+        darkergray: "#7E839A",
+        lightgray: "#F5F5FA",
+        lightblue: "#D6E4FF",
+        mineshaft: "#333333",
     },
     breakPoint: {
         mobileMax: 767,
