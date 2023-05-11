1c92f08efb8bc385e55472136424971973293372
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-11 05:12:56 +0200

    added colors and basic boxShadow to theme.js

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,12 +1,19 @@
 export const theme = {
   colors: {
     black: "#18181B",
-    blue: "#0044CC",
+    darkerGrey: "#7E839A",
+    lightGrey: "#F5F5FA",
+    grey: "#E4E6F0",
+    silver: "#C4C4C4",
     mercury: "#E5E5E5",
+    blue: "#0044CC",
     white: "#FFFFFF",
   },
   breakPoints: {
     phoneMax: "750px",
     tabletMax: "1450px",
   },
+  boxShadow: {
+    basic: "0px 4px 12px rgba(186, 199, 213, 0.5",
+  }
 };
