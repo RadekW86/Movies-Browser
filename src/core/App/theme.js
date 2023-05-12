a35df206ab210264bd2750d9a4b5f25637507d45
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-12 18:08:25 +0200

    added colors and lineHight to theme.js

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,5 +1,6 @@
 export const theme = {
   colors: {
+    solidBlack: "#000000",
     black: "#18181B",
     darkerGrey: "#7E839A",
     lightGrey: "#F5F5FA",
@@ -15,6 +16,12 @@ export const theme = {
     tabletMax: "1450px",
   },
   boxShadow: {
-    basic: "0px 4px 12px rgba(186, 199, 213, 0.5",
+    basic: "0px 4px 12px rgba(186, 199, 213, 0.5)",
+  },
+  lineHeight: {
+    xs: "120%",
+    s: "130%",
+    m: "150%",
+    l: "160%"
   }
 };
