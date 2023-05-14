fe6b1f7fa2ae27ea316df94f2c0123e5c34b3520
Jakub Jachacz <hexadoxpl@gmail.com>
2023-05-14 16:50:07 +0200

    update Search styling

--- src/common/Navigation/Search/styled.js
+++ src/common/Navigation/Search/styled.js
@@ -4,15 +4,13 @@ import SearchImage from "../../images/search.svg";
 export const Search = styled.input` 
 height: 48px;
 width: 432px;
-border-radius: 33px;
-margin-left: 380px;
-margin-right: 292px;
+border-radius: 33px; 
 border: 1px solid ${({ theme }) => theme.colors.gray};
 background-image: url(${SearchImage});
 background-repeat: no-repeat;
 background-position: 27px center;
-padding-left: 63px;
-background-size: auto;  
+padding-left: 63px; 
+align-self: center;
 
 &::placeholder {
     color: ${({ theme }) => theme.colors.darkergray}
