24219da9e5f74800b31c68aebad54879c55511aa
Radek Włodarski <wlodarski.radek@gmail.com>
2023-05-21 21:48:10 +0200

    corrected routes paths

--- src/core/routes.js
+++ src/core/routes.js
@@ -2,5 +2,5 @@ export const toMovies = () => "/Movies";
 export const toPeople = () => "/People";
 export const toExtras = () => "/Extras";
 
-export const toMovie = ( {id} = {id: ":id"}) => `/Movies/${id}`;
-export const toProfile = ( {id} = {id: ":id"}) => `/People/${id}`;
+export const toMovie = ( {id} = {id: ":id"}) => `/Movie/${id}`;
+export const toProfile = ( {id} = {id: ":id"}) => `/Profile/${id}`;
