bbc0ba04a30bbc309b6004dc76a5caf69aa8bcf4
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-22 13:40:45 +0200

    added watchFetchMoviePage to rootSaga.js

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,10 +1,12 @@
-import { watchFetchMovies } from "../features/movies/MoviesList/moviesSaga"
+import { watchFetchMovies } from "../features/movies/MoviesList/moviesSaga";
 import { watchFetchPeople } from "../features/people/peopleSaga";
+import { watchFetchMoviePage } from "../features/movies/MoviePage/moviePageSaga";
 import { all } from "redux-saga/effects";
 
 export default function* rootSaga() {
-    yield all([
-        watchFetchMovies(),
-        watchFetchPeople(),
-    ]);
+  yield all([
+    watchFetchMovies(), 
+    watchFetchMoviePage(), 
+    watchFetchPeople()
+]);
 };
