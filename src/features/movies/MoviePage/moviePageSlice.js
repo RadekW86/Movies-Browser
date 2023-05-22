2790f0d99c0be949ecccc50aff6cd0db0d91af21
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-22 14:42:02 +0200

    added moviePageSlice to MoviePage.js

---
+++ src/features/movies/MoviePage/moviePageSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const moviePageSlice = createSlice({
+  name: "moviePage",
+  initialState: {
+    movie: [],
+    movieCredits: [],
+  },
+  reducers: {
+    fetchMoviePageLoading: () => ({
+      movieState: "loading",
+    }),
+    fetchMoviePageSuccess: (_, { payload: movie }) => ({
+      movieState: "success",
+      movie,
+    }),
+    fetchMoviePageError: () => ({
+      movieState: "error",
+    }),
+    fetchMovieCreditsSuccess: (state, { payload: movieCredits }) => ({
+      ...state,
+      movieCreditsState: "success",
+      movieCredits,
+    }),
+  },
+});
+
+export const {
+  fetchMoviePageLoading,
+  fetchMoviePageSuccess,
+  fetchMoviePageError,
+  fetchMovieCreditsLoading,
+  fetchMovieCreditsSuccess,
+  fetchMovieCreditsError,
+} = moviePageSlice.actions;
+
+export const selectMoviePageSlice = (state) => state.moviePage;
+export const selectMoviePage = (state) => selectMoviePageSlice(state).movie;
+export const selectMoviePageState = (state) =>
+  selectMoviePageSlice(state).movieState;
+export const selectMovieCredits = (state) =>
+  selectMoviePageSlice(state).movieCredits;
+export const selectMovieCreditsState = (state) =>
+  selectMoviePageSlice(state).movieCreditsState;
+
+export default moviePageSlice.reducer;
