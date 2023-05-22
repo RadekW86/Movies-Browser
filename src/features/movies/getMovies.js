baa51c2553cd454a4205f7cb3e9ff118e5cf7fe2
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-22 15:36:03 +0200

    added getMovieCredits

--- src/features/movies/getMovies.js
+++ src/features/movies/getMovies.js
@@ -1,7 +1,29 @@
-export const getMoviesList = async (page) => {
-  const KEY_API = "445d0cbde8fc680f93bc54bd75a8924d";
-  const URL_API = `https://api.themoviedb.org/3/movie/popular?api_key=${KEY_API}&language=en-US&page=${page}`;
+import { KEY_API } from "../../common/KEY_API";
 
+export const getMoviesList = async (page) => {
+  const URL_API = `https://api.themoviedb.org/3/movie/popular?api_key=${KEY_API}&language=en-US&page=${page}`;
+  const response = await fetch(URL_API);
+
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+
+  return await response.json();
+};
+
+export const getMovie = async (movie_id) => {
+  const URL_API = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${KEY_API}&language=en-US`;
+  const response = await fetch(URL_API);
+
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+
+  return await response.json();
+};
+
+export const getMovieCredits = async (movie_id) => {
+  const URL_API = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${KEY_API}&language=en-US`;
   const response = await fetch(URL_API);
 
   if (!response.ok) {
