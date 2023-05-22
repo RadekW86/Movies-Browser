44bdcd53892565fcf5395b1ab2b9091b2863da2b
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-22 15:36:50 +0200

    added StyledLink to BasicTile in MoviesList

--- src/features/movies/MoviesList/index.js
+++ src/features/movies/MoviesList/index.js
@@ -2,7 +2,11 @@ import { Section } from "../../../common/Section";
 import { BasicTile } from "../../../common/content/BasicTile";
 import { Loading } from "../../../common/content/Loading";
 import { useSelector } from "react-redux";
-import { selectMovies, selectMoviesState, selectPage } from "../MoviesList/moviesSlice";
+import {
+  selectMovies,
+  selectMoviesState,
+  selectPage,
+} from "../MoviesList/moviesSlice";
 import { Error } from "../../../common/content/Error";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
@@ -10,13 +14,14 @@ import { fetchMoviesLoading } from "../MoviesList/moviesSlice";
 import { Pagination } from "../../../common/Pagination";
 import { Container } from "../../../common/Container/styled";
 import { useGetQueryParameter } from "../../../common/setQueryParameters";
+import { StyledLink } from "../MoviesList/styled";
 
 export const MoviesList = () => {
   const moviesState = useSelector(selectMoviesState);
   const moviesList = useSelector(selectMovies);
   const page = useSelector(selectPage);
   const dispatch = useDispatch();
-  const pageNumber = useGetQueryParameter("page")
+  const pageNumber = useGetQueryParameter("page");
 
   useEffect(() => {
     dispatch(fetchMoviesLoading(pageNumber));
@@ -36,16 +41,20 @@ export const MoviesList = () => {
             title="Popular Movies"
             foot={<Pagination currentPage={page} totalPages="500" />}
             content={moviesList.map((movie) => (
-              <BasicTile
-                movie
-                key={movie.id}
-                poster={movie.poster_path}
-                name={movie.title}
-                productionInF={movie.release_date}
-                genres={movie.genre_ids}
-                rate={movie.vote_average}
-                votes={movie.vote_count}
-              />
+              <StyledLink
+                to={`/Movies/${movie.id}`}
+              >
+                <BasicTile
+                  movie
+                  key={movie.id}
+                  poster={movie.poster_path}
+                  name={movie.title}
+                  productionInF={movie.release_date}
+                  genres={movie.genre_ids}
+                  rate={movie.vote_average}
+                  votes={movie.vote_count}
+                />
+              </StyledLink>
             ))}
           />
         </Container>
