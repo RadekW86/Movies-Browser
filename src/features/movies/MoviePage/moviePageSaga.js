ce25bbff3c23d5e380959429e6cba425c1bd0abe
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-22 14:42:55 +0200

    added MoviePageSaga.js to MoviePage

---
+++ src/features/movies/MoviePage/moviePageSaga.js
@@ -0,0 +1,23 @@
+import { put, call, takeLatest } from "redux-saga/effects";
+import {
+  fetchMovieCreditsSuccess,
+  fetchMoviePageError,
+  fetchMoviePageLoading,
+  fetchMoviePageSuccess,
+} from "./moviePageSlice";
+import { getMovie, getMovieCredits } from "../getMovies";
+
+function* watchFetchMoviePageHandler({ payload: movie_id }) {
+  try {
+    const movie = yield call(getMovie, movie_id);
+    const movieCredits = yield call(getMovieCredits, movie_id);
+    yield put(fetchMoviePageSuccess(movie));
+    yield put(fetchMovieCreditsSuccess(movieCredits));
+  } catch (error) {
+    yield put(fetchMoviePageError());
+  }
+}
+
+export function* watchFetchMoviePage() {
+  yield takeLatest(fetchMoviePageLoading.type, watchFetchMoviePageHandler);
+}
