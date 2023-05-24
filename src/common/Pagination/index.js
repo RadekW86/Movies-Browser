d711bf106ad2f0cc4c48f09933f9b57a00d60959
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-24 10:59:39 +0200

    added disabled conditions, disabled buttons styles and changed styled.js in Pagination

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -5,23 +5,35 @@ import {
   PageText,
   Pages,
   PaginationContainer,
+  StyledArrowLeft,
+  StyledArrowRight,
+  ArrowWrapper,
 } from "./styled";
 import { useSetQueryParameter } from "../setQueryParameters";
+
 export const Pagination = ({ currentPage, totalPages }) => {
   const page = parseInt(currentPage);
+  const firstPage = page === 1;
+  const lastPage = page === 500;
   const setQueryParameter = useSetQueryParameter();
   const setPage = (targetValue) => {
     setQueryParameter("page", targetValue);
   };
   return (
     <PaginationContainer>
-      <PageButton isFirst onClick={() => setPage(1)}>
+      <PageButton isFirst onClick={() => setPage(1)} disabled={firstPage}>
+        <StyledArrowLeft disabled={firstPage} />
+        <ArrowWrapper>
+          <StyledArrowLeft disabled={firstPage} />
+        </ArrowWrapper>
         <StyledSpan>First</StyledSpan>
       </PageButton>
       <PageButton
         isPrevious
-        onClick={() => setPage(page === 1 ? page : page - 1)}
+        onClick={() => setPage(page - 1)}
+        disabled={firstPage}
       >
+        <StyledArrowLeft disabled={firstPage} />
         <StyledSpan>Previous</StyledSpan>
       </PageButton>
       <Pages>
@@ -32,14 +44,20 @@ export const Pagination = ({ currentPage, totalPages }) => {
           of <PageCount id="totalPages">{totalPages}</PageCount>
         </PageText>
       </Pages>
-      <PageButton
-        isNext
-        onClick={() => setPage(page === 500 ? page : page + 1)}
-      >
+      <PageButton isNext onClick={() => setPage(page + 1)} disabled={lastPage}>
         <StyledSpan>Next</StyledSpan>
+        <StyledArrowRight disabled={lastPage} />
       </PageButton>
-      <PageButton isLast onClick={() => setPage(totalPages)}>
+      <PageButton
+        isLast
+        onClick={() => setPage(totalPages)}
+        disabled={lastPage}
+      >
         <StyledSpan>Last</StyledSpan>
+        <StyledArrowRight disabled={lastPage} />
+        <ArrowWrapper>
+          <StyledArrowRight disabled={lastPage} />
+        </ArrowWrapper>
       </PageButton>
     </PaginationContainer>
   );
