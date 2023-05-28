34dd8832dc857e8ba3d774d45f00e95ec48e8541
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-28 20:55:00 +0200

    fixed import movieGenresReducers

--- src/core/store.js
+++ src/core/store.js
@@ -3,7 +3,7 @@ import moviesReducers from "../features/movies/MoviesList/moviesSlice";
 import moviePageReducers from "../features/movies/MoviePage/moviePageSlice";
 import peopleReducers from "../features/people/PeopleList/peopleSlice";
 import profileReducers from "../features/people/Profile/profileSlice";
-import movieGenresReducers from "../features/movieGenresSlice";
+import movieGenresReducers from "../features/movies/movieGenresSlice";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 
