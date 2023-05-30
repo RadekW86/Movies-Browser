8020b8ccf375e3924f40571ebcd8c17661fda3e8
Radek Włodarski <wlodarski.radek@gmail.com>
2023-05-30 09:44:06 +0200

    one last conflict solved

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -5,16 +5,14 @@ import { watchFetchPeople } from "../features/people/PeopleList/peopleSaga";
 import { watchFetchProfile } from "../features/people/Profile/profileSaga";
 import { watchSearch } from "../TopBar/Search/searchSaga";
 import { watchFetchMovieGenres } from "../features/movies/movieGenresSaga";
-import { all } from "redux-saga/effects";
-
 
 export default function* rootSaga() {
   yield all([
+    watchFetchMovieGenres(),
     watchFetchMovies(),
     watchFetchMoviePage(),
     watchFetchPeople(),
     watchFetchProfile(),
     watchSearch(),
-    watchFetchMovieGenres(),
   ]);
 }
